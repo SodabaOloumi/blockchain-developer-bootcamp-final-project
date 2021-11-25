5bf5a57d8f2cea0e67196951a1a5c0ff1ae7bc36
Sodaba Oloumi <sodaba.ulomi@gmail.com>
2021-11-25 12:27:20 -0800

    change

--- vapp/deploy.sh
+++ vapp/deploy.sh
@@ -10,7 +10,6 @@ cd dist
 git init
 git add -A 
 git commit -m 'deploy'
-git push -f 
-git@github.com:SodabaOloumi/blockchain-developer-bootcamp-final-project/tree/main/vapp.git main:gh-pages
+git push -f https://github.com/SodabaOloumi/blockchain-developer-bootcamp-final-project.git main:gh-pages
 
 cd -
