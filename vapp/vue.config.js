9173cc74b38205816eb3296d622f10abb890c79f
Sodaba Oloumi <sodaba.ulomi@gmail.com>
2021-11-29 12:11:47 -0800

    change

--- vapp/vue.config.js
+++ vapp/vue.config.js
@@ -1,5 +1,5 @@
 module.exports ={
     publicPath: process.env.NODE_ENV === "production" 
-    ? "blockchain-developer-bootcamp-final-project/" 
+    ? "blockchain-developer-bootcamp-final-project" 
     : "/",
 }
